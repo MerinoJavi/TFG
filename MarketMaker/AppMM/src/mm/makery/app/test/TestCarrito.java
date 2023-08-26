753c74a9af08cd43bc373a9f6955d73f6a1b98e5
MerinoJavi <48442643+MerinoJavi@users.noreply.github.com>
2023-08-26 23:34:53 +0200

    Realización de nuevas pruebas

--- MarketMaker/AppMM/src/mm/makery/app/test/TestCarrito.java
+++ MarketMaker/AppMM/src/mm/makery/app/test/TestCarrito.java
@@ -1,25 +1,35 @@
 package mm.makery.app.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLSyntaxErrorException;
+import java.sql.Statement;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import mm.makery.app.view.LoginController;
+
 @RunWith(JUnit4.class)
 public class TestCarrito {
 	
-	double precioTotalEsperado=36.0;
+	double precioTotalEsperado=1338.3;
 	static double precioTest=0.0;
 	static Double precioTestNulo=0.0; //Variable para el tests que comprueba que el carrito es nulo
 	int idClient=254; //Cliente del primer test
@@ -28,8 +38,8 @@ public class TestCarrito {
 	String pass= "9P$H7nI5!*8p";
 	
 	//Prueba para comprobar el precio total de los productos del cliente con id 254.
-	public void getPrecioTotal() {
-		try {
+	public void getPrecioTotal() throws SQLException{
+		
 			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFG",root,pass);	
 			//Calculo el total del carrito del cliente
 				String productsQuery = "SELECT SUM(cantidad*precio_unitario) AS preciototal from carrito where id_cliente="+idClient;
@@ -44,14 +54,11 @@ public class TestCarrito {
 				totalSta.close();
 				conexion.close();
 			
-		} catch (SQLException e) { //Manejo de excepciones
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 	//Prueba en la cual se introduce un id de un cliente que no tiene ningun producto en el carrito
-	public void getPrecioTotalNulo() {
-		try {
+	public void getPrecioTotalNulo() throws SQLException {
+		
 			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFG", root, pass);	
 			//Calculo el total del carrito del cliente
 				String productsQuery = "SELECT SUM(cantidad*precio_unitario) AS preciototal from carrito where id_cliente="+idClientPrecioNulo;
@@ -67,10 +74,7 @@ public class TestCarrito {
 				totalSta.close();
 				conexion.close();
 			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 	//En este metodo simulo una caida de la base de datos, donde en el test se maneja y captura la excepcion SQLException que se lanza
 	public void getPrecioTotalError() throws SQLException {
@@ -124,22 +128,77 @@ public class TestCarrito {
 				totalSta.close();
 				conexion.close();
 	}
+	//Actualiza la cantidad del producto y le suma uno más
+	public void actualizarCantidadProducto() throws SQLException {
+		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFG", root, pass);
+		String quantityUpdateQuery = "UPDATE carrito SET cantidad= cantidad+1 WHERE id_producto=70";
+		PreparedStatement quantitySta = conexion.prepareStatement(quantityUpdateQuery);
+		quantitySta.execute();
+		
+		quantitySta.close();
+		conexion.close();
+	}
+	
+	//Se obtiene el precio del carrito de un cliente que no tiene productos en el carrito.id del cliente 375
+	public int totalCarritoVacioCliente() throws SQLException{
+		int total=0;
+		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFG", root, pass);	
+		//Calculo el total del carrito del cliente
+			String productsQuery = "SELECT SUM(cantidad*precio_unitario) AS preciototal from carrito  where id_cliente=375";
+			PreparedStatement totalSta = conexion.prepareStatement(productsQuery);
+			ResultSet totalSet = totalSta.executeQuery();
+			if(totalSet.next()) {
+				 total=totalSet.getInt("preciototal");
+			}
+			
+			
+			return total;
+	}
+	
+	//Elimina un producto del carrito con id de carrito 48 y producto 387
+	public void eliminarProducto() throws SQLException {
+		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFG", root, pass);	
+		String getProductoEliminarQuery = "DELETE FROM carrito where idcarrito=48 and id_producto=387";
+		Statement staEliminarProducto = conexion.prepareStatement(getProductoEliminarQuery);
+		
+	}
+	//sumar la cantidad total de productos del cliente con id 254 y comprobar el resultado
+	public int totalCantidadProducto() throws SQLException {
+		int totalcantidad=0;
+		Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFG", root, pass);	
+		//Calculo el total del carrito del cliente
+			String productsQuery = "SELECT SUM(cantidad) as totalcantidad from carrito where id_cliente=254";
+			PreparedStatement totalSta = conexion.prepareStatement(productsQuery);
+			ResultSet totalSet = totalSta.executeQuery();
+			if(totalSet.next()) {
+				 totalcantidad=totalSet.getInt("totalcantidad");
+			}
+			return totalcantidad;
+	}
 	//*****************************************************************TESTS************************************************************************************//
 																																																																					  //
 	//Comprobacion de precio del carrito de un cliente registrado en la base de datos																																					 //
 	@Test
 	public void precioTest() {
-		getPrecioTotal();
-		assertEquals(precioTotalEsperado, precioTest,0.001); //Usamos un delta (margen de error)
-		System.out.println("Test 1 correcto!");
+		try {
+			getPrecioTotal();
+			assertEquals(precioTotalEsperado, precioTest,0.001); //Usamos un delta (margen de error)
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("Error en la base de datos en el Test 1");
+		}
 		
 	}
 	//Test con cliente sin carrito.
 	@Test
 	public void testCarritoVacio() {
-		getPrecioTotalNulo();
-		assertNull("El carrito del cliente "+idClientPrecioNulo+" no está vacío", precioTestNulo);
-		System.out.println("Test 2 correcto!");
+		try {
+			getPrecioTotalNulo();
+			assertNull("El carrito del cliente "+idClientPrecioNulo+" no está vacío", precioTestNulo);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("Error en el test 2");
+		}
 	}
 	
 	//Simulacion de caida de la base de datos
@@ -150,7 +209,7 @@ public class TestCarrito {
 			fail("Excepcion no lanzada, error en el test");
 		}catch(SQLException e) {
 			assertEquals("No operations allowed after statement closed.", e.getMessage());
-			System.out.println("Test 3 correcto!");
+//			System.out.println("Test 3 correcto!");
 		}
 	}
 	//Test con un cliente de la base de datos que no está registrado
@@ -161,7 +220,7 @@ public class TestCarrito {
 			fail("Excepcion no lanzada, error en el test");
 		}catch(NullPointerException e) {
 			assertTrue("Excepcion no capturada", e instanceof NullPointerException);
-			System.out.println("Test 4 correcto!");
+//			System.out.println("Test 4 correcto!");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			fail("Se esperaba una excepcion NullPointerException");
@@ -171,9 +230,15 @@ public class TestCarrito {
 	//Test para comprobar que el precio es positivo
 	@Test
 	public void testTotalPositivo() {
-		getPrecioTotal();
-		assertTrue("El precio total no es un numero positivo", precioTest>0);
-		System.out.println("Test 5 correcto!");
+		try {
+			getPrecioTotal();
+			assertTrue("El precio total no es un numero positivo", precioTest>0);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("Error en el test 5");
+		}
+		
+//		System.out.println("Test 5 correcto!");
 	}
 	
 	//Test donde se captura la excepcion lanzada por sintaxis incorrecta
@@ -184,9 +249,56 @@ public class TestCarrito {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			assertTrue("La excepcion de sintaxis incorrecta no ha sido lanzada",e instanceof SQLSyntaxErrorException);
-			System.out.println("Test 6 correcto!");
+//			System.out.println("Test 6 correcto!");
 		}
 	}
 	//***************************************************************************************************************************************************************************//
+	//Actualizar la cantidad de un producto con id 70 del carrito del cliente con id de carrito 48
+	@Test
+	public void TestActualizarCantidadProducto() {
+		try {
+			actualizarCantidadProducto();
+//			System.out.println("Test 7 correcto!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("Excepcion no esperada");
+		}
+	}
+	//Test para calcular el total de un carrito vacio (por ende no tiene productos)
+	@Test
+	public void TestTotalCarritoVacio() {
+		try {
+			int total = totalCarritoVacioCliente();
+			assertEquals(total, 0);
+//			System.out.println("Test 8 correcto");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("No se esperaba este error");
+		}
+	}
+	//Elimina un producto del carrito con id de carrito 48 y producto 387
+	@Test
+	public void TestEliminacionProducto() {
+		try {
+			eliminarProducto();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("El producto no se ha podido eliminar");
+		}
+	}
+	//Test para sumar la cantidad total de productos del cliente con id 254 y comprobar el resultado
+	@Test
+	public void TestCantidadProducto() {
+		try {
+			int cantidad=totalCantidadProducto();
+			assertEquals(10, cantidad);
+//			System.out.println("Test 10 correcto!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			fail("Excepcion no esperada");
+			
+		}
+	}
 	
+
 }
